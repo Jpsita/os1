555a6ebf613c78e6a567546f10bbfd007833a6e3
jstfanelli <jpsdzn@gmail.com>
2017-06-16 15:44:45 +0200

    Debug now dosen't use TMUX

--- debug.sh
+++ debug.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-parent_path=$( cd "$(dirname "${BASH_SOURCE}")" ; pwd -P )
-cd "$parent_path"
-
-
-tmux new -s emulator -d "qemu-system-i386 -fda images/floppy.img -S -s -m 512M"
-sleep 2
-gdb  -command=gdbScript 
+gdb -x gdbScript
